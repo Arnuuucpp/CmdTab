295abb2aec39037e4132b17117c8dd06649fe958
Arnav <itsarnav3546@gmail.com>
2026-08-17 00:18:07 +0530

    feat: update Github component with project data and styling; add MacWindow styling

--- src/components/windows/Github.jsx
+++ src/components/windows/Github.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
-import MacWindow from './components/windows/MacWindow'
-import GithubData from  "../../assets/github.json"
+import React from "react";
+import MacWindow from "../windows/MacWindow.jsx";
+import GithubData from "../../assets/github.json";
+import "./github.scss";
 
-function GitCard({data}) {
-    
-}
+const GitCard = ({ data = { id: 1, image: "", title: "", description: "", tags: [], repoLink: "", demoLink: "" } }) => {
+    return (
+        <div className="git-card">
+            <img src={data.image} alt={data.title} className="git-card-image" />
 
+            <div className="git-card-body">
+                <h3>{data.title}</h3>
+                <p>{data.description}</p>
+
+                <div className="tags">
+                    {(data.tags || []).map((tag, index) => (
+                        <span key={`${tag}-${index}`} className="tag">{tag}</span>
+                    ))}
+                </div>
+
+                <div className="urls">
+                    {data.repoLink && (
+                        <a href={data.repoLink} target="_blank" rel="noreferrer">
+                            Repo
+                        </a>
+                    )}
+                    {data.demoLink && (
+                        <a href={data.demoLink} target="_blank" rel="noreferrer">
+                            Demo
+                        </a>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const Github = () => {
-  return (
-    <MacWindow>
-        <div>
-            {GithubData.map(projects => {
-                return <GitCard/>
-            })}
-        </div>
-    </MacWindow>
-  )
-}
+    return (
+        <MacWindow>
+            <div className="cards">
+                {GithubData.map((project, idx) => (
+                    <GitCard key={idx} data={project} />
+                ))}
+            </div>
+        </MacWindow>
+    );
+};
 
-export default Github
+export default Github;
--- src/components/windows/MacWindow.jsx
+++ src/components/windows/MacWindow.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import {Rnd} from "react-rnd"
+import { Rnd } from "react-rnd"
 import "./MacWindow.scss"
 
-const MacWindow = ({children}) => {
+const MacWindow = ({ children }) => {
   return (
     <Rnd>
-       <div  className='window'>
+      <div className='window'>
         <nav className='window-nav'>
-            <div className='dots'>
+          <div className='dots'>
             <div className="red-dot dot"></div>
             <div className="yellow-dot dot"></div>
             <div className="green-dot dot"></div>
-            </div>
-            <div className='title'>
-                <p>Arnav - zsh</p>
-            </div>
+          </div>
+          <div className='title'>
+            <p>Arnav - zsh</p>
+          </div>
         </nav>
         <div className='main-content'>
-            {children}
+          {children}
         </div>
-       </div>
+      </div>
     </Rnd>
   )
 }
